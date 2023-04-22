8b54296297effdc1a8108555d0f54d0206fa2768
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-22 16:52:29 -0500

    getting started

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>Welcome to my profile! I am an enthusiastic and dedicated entry-level web developer with a passion for creating dynamic and user-friendly websites. With a solid foundation in HTML, CSS, and JavaScript, I am excited to apply my skills to bring innovative ideas to life. As a detail-oriented individual, I thrive in fast-paced environments and enjoy collaborating with cross-functional teams to deliver high-quality solutions. I am constantly learning and staying up-to-date with the latest web development technologies and trends to ensure I am providing cutting-edge solutions. I am committed to delivering efficient and effective web solutions that exceed client expectations. Let's collaborate and create exceptional web experiences together!</p>
     </div>
   );
 }
