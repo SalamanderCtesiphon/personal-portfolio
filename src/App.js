01825c074e39ee6d7c986fde225a9cb31f896a7f
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-22 19:49:39 -0500

    added icons with links to my social media

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Header from './components/Header';
+import github from './icons/github-original-wordmark.svg'
+import linked from './icons/linkedin-original.svg'
+import twit from './icons/twitter-original.svg'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div className='icon-container'>hi</div>
-
+      <div className='icon-container'>
+      <a href='https://www.linkedin.com/in/shannon-brookshire-817577265/' ><img className='icons' alt='linked in link'src={linked}></img></a>
+      <a href="https://github.com/SalamanderCtesiphon/" ><img className='icons' alt='git hub link' src={github}></img></a>
+      <a href='https://twitter.com/Shannon99535365'><img className='icons' alt='twitter link' src={twit} ></img></a>
+      </div>
     </div>
   );
 }
