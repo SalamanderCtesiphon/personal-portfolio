3ce418d70c4aa4447d1b7247cd88090181021651
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-22 19:54:55 -0500

    fixed a typo

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ function Header() {
         </div>
         <div className='name'>Shannon Brookshire</div>
         <h1 className='intro'>About me</h1>
-        <p className='intro-para'>Welcome to my profile! I am an enthusiastic and dedicated entry-level web developer with a passion for creating dynamic and user-friendly websites. With a solid foundation in HTML, CSS and JavaScript - not to mention ReactJS and firebase, I am excited to apply my skills to bring innovative ideas to life. As a detail-oriented individual, I thrive in fast-paced environments and enjoy collaborating with cross-functional teams to deliver high-quality solutions. I am constantly learning and staying up-to-date with the latest web development technologies and trends to ensure I am providing cutting-edge solutions. </p>
+        <p className='intro-para'>Welcome to my profile! I am an enthusiastic and dedicated entry-level web developer with a passion for creating dynamic and user-friendly websites. With a solid foundation in HTML, CSS and JavaScript - not to mention ReactJS and Firebase, I am excited to apply my skills to bring innovative ideas to life. As a detail-oriented individual, I thrive in fast-paced environments and enjoy collaborating with cross-functional teams to deliver high-quality solutions. I am constantly learning and staying up-to-date with the latest web development technologies and trends to ensure I am providing cutting-edge solutions. </p>
         <div className='icon-container'>
           <a href='https://www.linkedin.com/in/shannon-brookshire-817577265/' ><img className='icons' alt='linked in link'src={linked}></img></a>
           <a href="https://github.com/SalamanderCtesiphon/" ><img className='icons' alt='git hub link' src={github}></img></a>
