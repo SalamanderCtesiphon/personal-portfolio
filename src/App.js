3aa2062ec52c48d41127b7b91017f729b48488ca
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-22 20:32:59 -0500

    added a work section to the page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import WorkSection from './components/WorkSection';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <WorkSection />
     </div>
   );
 }
