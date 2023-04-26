42311b2f5ef814b8b682211ce89e63a163910e77
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-26 11:56:13 -0500

    redesign

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
+    <div className='header section'>
       <h1>Hi This is the title</h1>
      </div>
   )
--- src/components/WorkSection.js
+++ src/components/WorkSection.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import resturant from '../images/resturant-page.png'
-import resume from '../images/resume-app.png'
-import memory from '../images/memory-game.png'
 
 function WorkSection() {
   return (
-    <div className='work-section'>
+    <div className='work-section section'>
       <p>This is the work section</p>
     </div>
   )
