7a5bbf90414eaee96adc2c598f5b04c4050d4a8c
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-26 13:00:39 -0500

    adjusted the layout to be more resposive

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,6 +4,12 @@ import layeredWaves from "../images/layered-waves-haikei.svg"
 function Hero() {
   return (
     <div className='hero section'>
+      <div className='title-container'>
+        <span className='hero-text' id='first-name'><h1>Shannon</h1></span>
+        <span className='hero-text' id='last-name'><h1>Brookshire</h1></span>
+        <span className='hero-text' id='title-one'><h1>Web</h1></span>
+        <span className='hero-text' id='title-two'><h1>Developer</h1></span>
+      </div>
       <img src={layeredWaves} alt='background' id='hero-background'></img>
     </div>
   )
