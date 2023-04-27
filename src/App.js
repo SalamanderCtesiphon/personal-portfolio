bdd64dbe27c51b96f5f0a34bee300dc8ddec6093
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-27 15:20:11 -0500

    tried to implement a sticky header

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Info from './components/Info';
 import WorkSection from './components/WorkSection';
 
 
 function App() {
- 
 
   return (
     <div className="App">
       <Header />
       <Hero />
       <WorkSection />
+      <Info />
       <Footer />
     </div>
   );
