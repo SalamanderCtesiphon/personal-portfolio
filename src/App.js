d1739ee7cba13f75b9193b62455e48348934cddf
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-27 15:51:51 -0500

    added a sticky header to the app

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Info from './components/Info';
 import WorkSection from './components/WorkSection';
+import Sticky from 'react-stickynode';
 
 
 function App() {
 
   return (
     <div className="App">
-      <Header />
+      <Sticky enabled={true} bottomBoundary={3600}><Header /></Sticky>
       <Hero />
       <WorkSection />
       <Info />
