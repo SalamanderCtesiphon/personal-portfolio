9c80e2879e712326c3cfcb41029eee3d183e8f87
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-27 16:40:29 -0500

    adjusted the styling

--- src/components/WorkSection.js
+++ src/components/WorkSection.js
@@ -10,17 +10,17 @@ function WorkSection() {
       <div className='work-section section'>
         <div className='work-card'>
           <h1 className='card-title'>Resturant Page</h1>
-          <img src={resturantPage} alt='resturant'></img>
+          <img src={resturantPage} alt='resturant' className='image'></img>
           <div className='work-text'>This is a link to the page</div>
         </div>
         <div className='work-card'>
           <h1 className='card-title'>Memory Game</h1>
-          <img src={memoryGame} alt='memory game'></img>
+          <img src={memoryGame} alt='memory game' className='image'></img>
           <div className='work-text'>This is a link to the page</div>
         </div>
         <div className='work-card'>
           <h1 className='card-title'>Resume App</h1>
-          <img src={resumeApp} alt='resume app'></img>
+          <img src={resumeApp} alt='resume app' className='image'></img>
           <div className='work-text'>This is a link to the page</div>
         </div>
       </div>
