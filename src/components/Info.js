4ac1bc75604a969e1244ecfe1fd7c5e74665cbab
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-27 16:18:10 -0500

    adde text to the section

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Info() {
   return (
-    <div className='info-section'> Info</div>
+    <div className='info-section'> 
+      <h1>Introduction</h1>
+      <h3>Hi! I'm Shannon</h3>
+      <p>I am a self-taught front-end web developer. I learned my skills by completeing the Odin Project, an on-line open-sourced curriculum curated by professinal web developers.</p>
+      <p>It required a signficant amout of grit and determination to complete the course. Along the way, I learned many valuable skills and concepts which I feel have prepared me for a carreer in web developement.</p>
+      <p>I have also developed a strong work ethic and a growing curiosity that motivates me tol learn more and improve my skills.</p>
+      <h5>A few of the subjects that I have worked with:</h5>
+      <div className='icon-container'></div>
+    </div>
   )
 }
 
