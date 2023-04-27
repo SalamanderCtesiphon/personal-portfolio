ec4f90edd82b1a8d06f74f5b8d7da82e91a4d5a7
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-27 13:44:50 -0500

    styled the portfolio section of the page

--- src/components/WorkSection.js
+++ src/components/WorkSection.js
@@ -5,18 +5,24 @@ import resumeApp from '../images/resume-app.png'
 
 function WorkSection() {
   return (
-    <div className='work-section section'>
-      <div className='work-card'>
-        <img src={resturantPage} alt='resturant'></img>
-        <div className='work-text'>This is a link to the page</div>
-      </div>
-      <div className='work-card'>
-        <img src={memoryGame} alt='memory game'></img>
-        <div className='work-text'>This is a link to the page</div>
-      </div>
-      <div className='work-card'>
-        <img src={resumeApp} alt='resume app'></img>
-        <div className='work-text'>This is a link to the page</div>
+    <div className='page'>
+      <h1>Portfolio</h1>
+      <div className='work-section section'>
+        <div className='work-card'>
+          <h1 className='card-title'>Resturant Page</h1>
+          <img src={resturantPage} alt='resturant'></img>
+          <div className='work-text'>This is a link to the page</div>
+        </div>
+        <div className='work-card'>
+          <h1 className='card-title'>Memory Game</h1>
+          <img src={memoryGame} alt='memory game'></img>
+          <div className='work-text'>This is a link to the page</div>
+        </div>
+        <div className='work-card'>
+          <h1 className='card-title'>Resume App</h1>
+          <img src={resumeApp} alt='resume app'></img>
+          <div className='work-text'>This is a link to the page</div>
+        </div>
       </div>
     </div>
   )
