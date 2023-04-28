07060ea202ad3ff6a3207bfa1f6146d9d76317c4
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-28 09:56:37 -0500

    styled the info section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='footer' id='footer-anchor'> Footer</div>
+    <div className='footer' id='footer-anchor'> 
+      <h1 className='paragraph'>Contact</h1>
+      <p className='paragraph'>I am currently looking for opportunities if you have a question or just want to chat, feel free to send me a message and I will get back to you!</p>
+    </div>
   )
 }
 
